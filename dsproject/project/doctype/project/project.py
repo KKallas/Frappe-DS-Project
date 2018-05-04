64a96956641b314b37c1a0adbdc08b2a343266da
Kaspar Kallas <kaspar@digitalsputnik.com>
2018-05-04 10:45:32 +0300

    "state" bug in prjoject frm

--- dsproject/project/doctype/project/project.py
+++ dsproject/project/doctype/project/project.py
@@ -17,7 +17,7 @@ class Project(Document):
 def getTaskCount(name):
 	tasks_total = frappe.db.sql("""select name from `tabDS Task` where prj=%s""",name)
 	#TODO change the filter to DS Task state based
-	tasks_open = frappe.db.sql("""select name from `tabDS Task` where prj=%s and state="Closed" """,name)
+	tasks_open = frappe.db.sql("""select name from `tabDS Task` where prj=%s and prjstate="Done" """,name)
 	return [len(tasks_total),len(tasks_open)]
 
 #to automatically set the same department as project for the DS Task
