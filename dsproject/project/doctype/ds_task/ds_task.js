643137a6b47c68a2772f032e1a8bc4877bd81824
Kaspar Kallas <kaspar@digitalsputnik.com>
2018-06-18 07:09:28 +0300

    Remove submittable and automatic project selection

--- dsproject/project/doctype/ds_task/ds_task.js
+++ dsproject/project/doctype/ds_task/ds_task.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2018, digitalsputnik and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DS Task', {
+/*frappe.ui.form.on('DS Task', {
 	refresh: function(frm) {
 		//how to set a value - default functionality
 		//frm.set_value("deadline","01-05-2018");
@@ -10,15 +10,15 @@ frappe.ui.form.on('DS Task', {
 
 		//kui on uus dokk
 
-	},
+	//},
 	//updateing prj will take the department value from prj
-	prj: function(frm) {
-		frappe.call({
-    	method:"dsproject.project.doctype.project.project.getDept",
-    	args:{"name":frm.doc.prj},
-			callback: function(r) {
-				frm.set_value("dept", r.message);
-    	}
+	//prj: function(frm) {
+	//	frappe.call({
+  //  	method:"dsproject.project.doctype.project.project.getDept",
+  //  	args:{"name":frm.doc.prj},
+	//		callback: function(r) {
+	//			frm.set_value("dept", r.message);
+  //  	}
 		});
 	},
-});
+});*/
